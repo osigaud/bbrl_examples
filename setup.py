8d965b5062f2d3c4418190215199dc9517f1d639
osigaud <Olivier.Sigaud@upmc.fr>
2022-04-04 12:52:16 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="my_gym",
-    packages=[package for package in find_packages() if package.startswith("my_gym")],
+    name="my_salina_examples",
+    packages=[package for package in find_packages() if package.startswith("my_salina_examples")],
     version="0.0.1",
     install_requires=["gym==0.21.0", "numpy>=1.19.1"],
     tests_require=["pytest==4.4.1"],
     test_suite="tests",
-    description="Additional gym library",
+    description="Examples of RL code with SaLinA",
     author="Olivier Sigaud",
     license="MIT",
 )
