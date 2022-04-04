fb2a321b51833fdcf4fd4d41dbe1f96b7f1e6b2c
osigaud <Olivier.Sigaud@upmc.fr>
2022-04-04 15:35:04 +0200

    added first models

---
+++ models/salina_shared_models.py
@@ -0,0 +1,12 @@
+from salina import Agent
+import torch
+import torch.nn as nn 
+
+
+def mlp(sizes, activation, output_activation=nn.Identity):
+    layers = []
+    for j in range(len(sizes) - 1):
+        act = activation if j < len(sizes) - 2 else output_activation
+        layers += [nn.Linear(sizes[j], sizes[j + 1]), act()]
+    m=nn.Sequential(*layers)
+    return m
