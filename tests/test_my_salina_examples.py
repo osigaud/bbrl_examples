867a537324bae0f1e909eff434cbf667a98ae781
osigaud <Olivier.Sigaud@upmc.fr>
2022-04-05 13:04:16 +0200

    added test

---
+++ tests/test_my_salina_examples.py
@@ -0,0 +1,30 @@
+from my_salina_examples.models.salina_actors import ProbAgent, ActionAgent
+from my_salina_examples.models.salina_critics import VAgent, QAgent
+from my_salina_examples.models.salina_envs import EnvAgent
+
+cfg = {
+    "logger": {
+        "classname": "salina.logger.TFLogger",
+        "log_dir": "./tmp",
+        "cache_size": 10000,
+        "every_n_seconds": 10,
+        "verbose": False,
+    },
+    "algorithm": {
+        "env_seed": 432,
+        "n_envs": 8,
+        "n_timesteps": 16,
+        "max_epochs": 10000,
+        "discount_factor": 0.95,
+        "entropy_coef": 0.001,
+        "critic_coef": 1.0,
+        "a2c_coef": 0.1,
+        "architecture": {"hidden_size": 32},
+        "env": {"classname": "__main__.make_env", "env_name": "CartPole-v1", "max_episode_steps": 100},
+        "optimizer": {"classname": "torch.optim.Adam", "lr": 0.01},
+    },
+}
+
+envAgent = EnvAgent(cfg)
+pa = ProbAgent(cfg)
+aa = ActionAgent(cfg)
