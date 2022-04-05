c5cb583afd50167d0edf552ec42a9625dfb5ecf7
osigaud <Olivier.Sigaud@upmc.fr>
2022-04-05 12:27:27 +0200

    modified env agents

--- my_salina_examples/models/salina_envs.py
+++ my_salina_examples/models/salina_envs.py
@@ -8,7 +8,7 @@ class EnvAgent(GymAgent):
     # Create the environment agent
     # This agent implements N gym environments with auto-reset
     def __init__(self, cfg):
-        super().__init__(get_class(cfg.env), get_arguments(cfg.gym_env), n_envs=cfg.algorithm.n_envs)
+        super().__init__(get_class(cfg.gym_env), get_arguments(cfg.gym_env), n_envs=cfg.algorithm.n_envs)
         env = instantiate_class(cfg.gym_env)
         self.observation_space = env.observation_space
         self.action_space = env.action_space
@@ -31,7 +31,7 @@ class AutoResetEnvAgent(AutoResetGymAgent):
     # Create the environment agent
     # This agent implements N gym environments with auto-reset
     def __init__(self, cfg, n_envs):
-        super().__init__(get_class(cfg.env), get_arguments(cfg.gym_env), n_envs=cfg.algorithm.n_envs)
+        super().__init__(get_class(cfg.gym_env), get_arguments(cfg.gym_env), n_envs=cfg.algorithm.n_envs)
         env = instantiate_class(cfg.gym_env)
         self.observation_space = env.observation_space
         self.action_space = env.action_space
@@ -54,7 +54,7 @@ class NoAutoResetEnvAgent(NoAutoResetGymAgent):
     # Create the environment agent
     # This agent implements N gym environments with auto-reset
     def __init__(self, cfg, n_envs):
-        super().__init__(get_class(cfg.env), get_arguments(cfg.gym_env), n_envs=cfg.algorithm.n_envs)
+        super().__init__(get_class(cfg.gym_env), get_arguments(cfg.gym_env), n_envs=cfg.algorithm.n_envs)
         env = instantiate_class(cfg.gym_env)
         self.observation_space = env.observation_space
         self.action_space = env.action_space
