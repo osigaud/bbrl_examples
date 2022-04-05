5b24a8b7ea8ba37640631bc7ec7195b50c91143a
osigaud <Olivier.Sigaud@upmc.fr>
2022-04-05 13:03:12 +0200

    imported local version of colab

---
+++ my_salina_examples/models/salina_loggers.py
@@ -0,0 +1,15 @@
+from salina import instantiate_class
+
+class Logger():
+
+  def __init__(self, cfg):
+    self.logger = instantiate_class(cfg.logger)
+
+  def add_log(self, log_string, loss, epoch):
+    self.logger.add_scalar(log_string, loss.item(), epoch)
+
+  # Log losses
+  def log_losses(self, cfg, epoch, critic_loss, entropy_loss, a2c_loss):
+    self.add_log("critic_loss", critic_loss, epoch)
+    self.add_log("entropy_loss", entropy_loss, epoch)
+    self.add_log("a2c_loss", a2c_loss, epoch)
