172cf6e6ef0604fedd5d3b34563928a53c459c9e
osigaud <Olivier.Sigaud@upmc.fr>
2022-04-11 11:14:12 +0200

    a2c continuous running

--- my_salina_examples/run_launcher/a2c/a2c_continuous_main.py
+++ my_salina_examples/run_launcher/a2c/a2c_continuous_main.py
@@ -19,7 +19,7 @@ import torch.nn as nn
 import torch.autograd as autograd
 from torch.autograd import detect_anomaly
 
-from my_salina_examples.models.salina_actors import ProbAgent, ActionAgent
+from my_salina_examples.models.salina_actors import ContinuousActionTunableVarianceAgent
 from my_salina_examples.models.salina_critics import VAgent
 from my_salina_examples.models.salina_envs import AutoResetEnvAgent
 from my_salina_examples.models.salina_loggers import Logger
@@ -27,30 +27,17 @@ from my_salina_examples.models.salina_loggers import Logger
 
 # Create the A2C Agent
 def create_a2c_agent(cfg, env_agent):
-    observation_size, n_actions = env_agent.get_obs_and_actions_sizes()
+    observation_size, action_dim = env_agent.get_obs_and_actions_sizes()
     # print(observation_size, n_actions)
-    prob_agent = ProbAgent(observation_size, cfg.algorithm.architecture.hidden_size, n_actions)
-    action_agent = ActionAgent()
+    action_agent = ContinuousActionTunableVarianceAgent(observation_size, cfg.algorithm.architecture.hidden_size, action_dim)
     critic_agent = VAgent(observation_size, cfg.algorithm.architecture.hidden_size)
 
     # Combine env and policy agents
-    agent = Agents(env_agent, prob_agent, action_agent)
+    agent = Agents(env_agent, action_agent)
     # Get an agent that is executed on a complete workspace
     agent = TemporalAgent(agent)
     agent.seed(cfg.algorithm.seed)
-    return agent, prob_agent, critic_agent
-
-
-def _index(tensor_3d, tensor_2d):
-    """
-    This function is used to index a 3d tensors using a 2d tensor
-    """
-    x, y, z = tensor_3d.size()
-    t = tensor_3d.reshape(x * y, z)
-    tt = tensor_2d.reshape(x * y)
-    v = t[torch.arange(x * y), tt]
-    v = v.reshape(x, y)
-    return v
+    return agent, action_agent, critic_agent
 
 
 def make_gym_env(max_episode_steps, env_name):
@@ -58,9 +45,9 @@ def make_gym_env(max_episode_steps, env_name):
 
 
 # Configure the optimizer over the a2c agent
-def setup_optimizers(cfg, prob_agent, critic_agent):
+def setup_optimizers(cfg, action_agent, critic_agent):
     optimizer_args = get_arguments(cfg.optimizer)
-    parameters = nn.Sequential(prob_agent, critic_agent).parameters()
+    parameters = nn.Sequential(action_agent, critic_agent).parameters()
     optimizer = get_class(cfg.optimizer)(parameters, **optimizer_args)
     return optimizer
 
@@ -76,8 +63,7 @@ def compute_critic_loss(cfg, reward, done, critic):
     return critic_loss, td
 
 
-def compute_a2c_loss(action_probs, action, td):
-    action_logp = _index(action_probs, action).log()
+def compute_a2c_loss(action_logp, td):
     a2c_loss = action_logp[:-1] * td.detach()
     return a2c_loss.mean()
 
@@ -90,7 +76,7 @@ def run_a2c(cfg, max_grad_norm=0.5):
     env_agent = AutoResetEnvAgent(cfg)
 
     # 3) Create the A2C Agent
-    a2c_agent, prob_agent, critic_agent = create_a2c_agent(cfg, env_agent)
+    a2c_agent, action_agent, critic_agent = create_a2c_agent(cfg, env_agent)
 
     # 4) Create the temporal critic agent to compute critic values over the workspace
     tcritic_agent = TemporalAgent(critic_agent)
@@ -102,7 +88,7 @@ def run_a2c(cfg, max_grad_norm=0.5):
     workspace = Workspace()
 
     # 6) Configure the optimizer over the a2c agent
-    optimizer = setup_optimizers(cfg, prob_agent, critic_agent)
+    optimizer = setup_optimizers(cfg, action_agent, critic_agent)
 
     # 7) Training loop
     for epoch in range(cfg.algorithm.max_epochs):
@@ -118,7 +104,7 @@ def run_a2c(cfg, max_grad_norm=0.5):
         tcritic_agent(workspace, n_steps=cfg.algorithm.n_timesteps)
 
         # Get relevant tensors (size are timestep x n_envs x ....)
-        critic, done, action_probs, reward, action = workspace["critic", "env/done", "action_probs", "env/reward", "action"]
+        critic, done, action_logp, reward, action = workspace["critic", "env/done", "action_logprobs", "env/reward", "action"]
         # print(action.flatten())
         # print(reward)
 
@@ -130,7 +116,7 @@ def run_a2c(cfg, max_grad_norm=0.5):
         entropy_loss = torch.mean(workspace['entropy'])
 
         # Compute A2C loss
-        a2c_loss = compute_a2c_loss(action_probs, action, td)
+        a2c_loss = compute_a2c_loss(action_logp, td)
         # print(a2c_loss.mean())
 
         # Store the losses for tensorboard display
@@ -145,7 +131,7 @@ def run_a2c(cfg, max_grad_norm=0.5):
 
         optimizer.zero_grad()
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(prob_agent.parameters(), max_grad_norm)
+        torch.nn.utils.clip_grad_norm_(action_agent.parameters(), max_grad_norm)
         optimizer.step()
 
         # Compute the cumulated reward on final_state
@@ -174,9 +160,10 @@ params = {
         "architecture": {"hidden_size": [24, 36]},
     },
     "gym_env": {"classname": "__main__.make_gym_env",
-                "env_name": "CartPole-v1",
+                "env_name": "CartPoleContinuous-v1",
                 "max_episode_steps": 500},
-    "optimizer": {"classname": "torch.optim.Adam", "lr": 0.01},
+    "optimizer": {"classname": "torch.optim.Adam",
+                  "lr": 0.01},
 }
 
 if __name__ == "__main__":
