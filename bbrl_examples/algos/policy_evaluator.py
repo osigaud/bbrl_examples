fixed issue in PPO KL

bbrl_examples/algos/policy_evaluator.py
@@ -9,14 +9,14 @@ from bbrl.agents import Agents, PrintAgent, TemporalAgent
 from bbrl.agents.gymb import NoAutoResetGymAgent
 from bbrl.utils.chrono import Chrono
 
-from bbrl_examples.models.actors import *
-from bbrl_examples.models.critics import *
+# from bbrl_examples.models.actors import *
+# from bbrl_examples.models.critics import *
+# from script_ddqn import *
 
 path = "/data/policies/"
 nb_trials = 2
 seed = 3
 
-from script_ddqn import *
 
 def make_gym_env(env_name):
     """Create the used environment"""
@@ -40,7 +40,7 @@ def evaluate_agent(filename, env_name):
         agents = Agents(eval_env, agent)
         eval_agent = TemporalAgent(agents)
     print(eval_agent)
-    
+
     means = np.zeros(nb_trials)
     for i in range(nb_trials):
         eval_workspace = Workspace()  # Used for evaluation

bbrl_examples/algos/ppo/ppo_clip.py
@@ -16,6 +16,7 @@ from bbrl import get_arguments, get_class
 from bbrl.utils.functionalb import gae
 
 from bbrl_examples.models.loggers import Logger
+
 # The workspace is the main class in BBRL, this is where all data is collected and stored
 from bbrl.workspace import Workspace
 
@@ -41,8 +42,10 @@ from bbrl.utils.chrono import Chrono
 from bbrl.visu.visu_policies import plot_policy
 from bbrl.visu.visu_critics import plot_critic
 
+
 def make_gym_env(env_name):
     return gym.make(env_name)
+
 
 def create_ppo_agent(cfg, train_env_agent, eval_env_agent):
     obs_size, act_size = train_env_agent.get_obs_and_actions_sizes()
@@ -72,11 +75,13 @@ def create_ppo_agent(cfg, train_env_agent, eval_env_agent):
 
     return policy, train_agent, eval_agent, critic_agent, old_policy, old_critic_agent
 
+
 def setup_optimizer(cfg, action_agent, critic_agent):
     optimizer_args = get_arguments(cfg.optimizer)
     parameters = nn.Sequential(action_agent, critic_agent).parameters()
     optimizer = get_class(cfg.optimizer)(parameters, **optimizer_args)
     return optimizer
+
 
 def compute_advantage(cfg, reward, must_bootstrap, v_value):
     # Compute temporal difference with GAE
@@ -89,20 +94,22 @@ def compute_advantage(cfg, reward, must_bootstrap, v_value):
     )
     return advantage
 
+
 def compute_critic_loss(advantage):
     td_error = advantage**2
     critic_loss = td_error.mean()
     return critic_loss
 
+
 def compute_clip_agent_loss(cfg, advantage, ratio):
-    """Computes the PPO CLIP loss
-    """
+    """Computes the PPO CLIP loss"""
     clip_range = cfg.algorithm.clip_range
 
     actor_loss_1 = advantage * ratio
     actor_loss_2 = advantage * torch.clamp(ratio, 1 - clip_range, 1 + clip_range)
     actor_loss = torch.minimum(actor_loss_1, actor_loss_2).mean()
     return actor_loss
+
 
 def run_ppo_v2(cfg):
     # 1)  Build the  logger
@@ -123,7 +130,7 @@ def run_ppo_v2(cfg):
     ) = create_ppo_agent(cfg, train_env_agent, eval_env_agent)
 
     actor = TemporalAgent(policy)
-    old_actor= TemporalAgent(old_policy)
+    old_actor = TemporalAgent(old_policy)
     train_workspace = Workspace()
 
     # Configure the optimizer
@@ -193,32 +200,40 @@ def run_ppo_v2(cfg):
         # then we compute the advantage using the clamped critic values
         advantage = compute_advantage(cfg, reward, must_bootstrap, v_value)
 
-
         # We store the advantage into the transition_workspace
         transition_workspace.set("advantage", 0, advantage)
         transition_workspace.set("advantage", 1, torch.zeros_like(advantage))
         # We rename logprob_predict data into old_action_logprobs
         # We do so because we will rewrite in the logprob_predict variable in mini_batches
-        transition_workspace.set_full("old_action_logprobs", transition_workspace["logprob_predict"].detach())
+        transition_workspace.set_full(
+            "old_action_logprobs", transition_workspace["logprob_predict"].detach()
+        )
         transition_workspace.clear("logprob_predict")
 
         # We start several optimization epochs on mini_batches
         for opt_epoch in range(cfg.algorithm.opt_epochs):
             if cfg.algorithm.minibatch_size > 0:
-                sample_workspace = transition_workspace.sample_subworkspace(1, cfg.algorithm.minibatch_size, 2)
+                sample_workspace = transition_workspace.sample_subworkspace(
+                    1, cfg.algorithm.minibatch_size, 2
+                )
             else:
                 sample_workspace = transition_workspace
 
-            actor(sample_workspace, t=0, n_steps=1, compute_entropy=True, predict_proba=True)
+            actor(
+                sample_workspace,
+                t=0,
+                n_steps=1,
+                compute_entropy=True,
+                predict_proba=True,
+            )
 
             advantage, action_logp, old_action_logp, entropy = sample_workspace[
-                "advantage",
-                "logprob_predict",
-                "old_action_logprobs",
-                "entropy"
+                "advantage", "logprob_predict", "old_action_logprobs", "entropy"
             ]
 
-            critic_loss = compute_critic_loss(advantage)  # issue here, can be used only once
+            critic_loss = compute_critic_loss(
+                advantage
+            )  # issue here, can be used only once
             adv_actor = advantage.detach().squeeze(0)[0]
             act_diff = action_logp[0] - old_action_logp[0]
             ratios = act_diff.exp()
@@ -236,8 +251,8 @@ def run_ppo_v2(cfg):
             loss_critic = cfg.algorithm.critic_coef * critic_loss
 
             loss = (
-                    - cfg.algorithm.actor_coef * actor_loss
-                    - cfg.algorithm.entropy_coef * entropy_loss
+                -cfg.algorithm.actor_coef * actor_loss
+                - cfg.algorithm.entropy_coef * entropy_loss
             )
 
             old_policy.copy_parameters(policy)
@@ -300,7 +315,13 @@ def run_ppo_v2(cfg):
                 directory = f"./ppo_agent/{cfg.gym_env.env_name}/"
                 if not os.path.exists(directory):
                     os.makedirs(directory)
-                filename = directory + cfg.gym_env.env_name + "#ppo_clip#team#" + str(mean.item()) + ".agt"
+                filename = (
+                    directory
+                    + cfg.gym_env.env_name
+                    + "#ppo_clip#team#"
+                    + str(mean.item())
+                    + ".agt"
+                )
                 policy.save_model(filename)
                 if cfg.plot_agents:
                     plot_policy(
@@ -318,6 +339,7 @@ def run_ppo_v2(cfg):
                         cfg.gym_env.env_name,
                         best_reward,
                     )
+
 
 @hydra.main(
     config_path="./configs/",

bbrl_examples/algos/ppo/ppo_kl.py
@@ -43,23 +43,9 @@ from bbrl.utils.chrono import Chrono
 from bbrl.visu.visu_policies import plot_policy
 from bbrl.visu.visu_critics import plot_critic
 
+
 def make_gym_env(env_name):
     return gym.make(env_name)
-
-def get_env_agents(cfg):
-    train_env_agent = AutoResetGymAgent(
-        get_class(cfg.gym_env),
-        get_arguments(cfg.gym_env),
-        cfg.algorithm.n_envs,
-        cfg.algorithm.seed,
-    )
-    eval_env_agent = NoAutoResetGymAgent(
-    get_class(cfg.gym_env),
-    get_arguments(cfg.gym_env),
-    cfg.algorithm.nb_evals,
-    cfg.algorithm.seed,
-    )
-    return train_env_agent, eval_env_agent
 
 
 # Create the PPO Agent
@@ -87,13 +73,23 @@ def create_ppo_agent(cfg, train_env_agent, eval_env_agent):
     old_policy = copy.deepcopy(policy)
     old_critic_agent = copy.deepcopy(critic_agent)
     kl_agent = TemporalAgent(KLAgent(old_policy, policy))
-    return policy, train_agent, eval_agent, critic_agent, old_policy, old_critic_agent, kl_agent
+    return (
+        policy,
+        train_agent,
+        eval_agent,
+        critic_agent,
+        old_policy,
+        old_critic_agent,
+        kl_agent,
+    )
+
 
 def setup_optimizer(cfg, action_agent, critic_agent):
     optimizer_args = get_arguments(cfg.optimizer)
     parameters = nn.Sequential(action_agent, critic_agent).parameters()
     optimizer = get_class(cfg.optimizer)(parameters, **optimizer_args)
     return optimizer
+
 
 def compute_advantage(cfg, reward, must_bootstrap, v_value):
     # Compute temporal difference with GAE
@@ -106,16 +102,18 @@ def compute_advantage(cfg, reward, must_bootstrap, v_value):
     )
     return advantage
 
+
 def compute_critic_loss(advantage):
     td_error = advantage**2
     critic_loss = td_error.mean()
     return critic_loss
 
+
 def compute_agent_loss(cfg, advantage, ratio, kl_loss):
-    """Computes the PPO loss including KL regularization
-    """
+    """Computes the PPO loss including KL regularization"""
     actor_loss = (advantage * ratio - cfg.algorithm.beta * kl_loss).mean()
     return actor_loss
+
 
 def run_ppo_v1(cfg):
     # 1)  Build the  logger
@@ -137,7 +135,7 @@ def run_ppo_v1(cfg):
     ) = create_ppo_agent(cfg, train_env_agent, eval_env_agent)
 
     actor = TemporalAgent(policy)
-    old_actor= TemporalAgent(old_policy)
+    old_actor = TemporalAgent(old_policy)
     train_workspace = Workspace()
 
     # Configure the optimizer
@@ -207,41 +205,47 @@ def run_ppo_v1(cfg):
         # then we compute the advantage using the clamped critic values
         advantage = compute_advantage(cfg, reward, must_bootstrap, v_value)
 
-
         # We store the advantage into the transition_workspace
         transition_workspace.set("advantage", 0, advantage)
         transition_workspace.set("advantage", 1, torch.zeros_like(advantage))
         # We rename logprob_predict data into old_action_logprobs
         # We do so because we will rewrite in the logprob_predict variable in mini_batches
-        transition_workspace.set_full("old_action_logprobs", transition_workspace["logprob_predict"].detach())
+        transition_workspace.set_full(
+            "old_action_logprobs", transition_workspace["logprob_predict"].detach()
+        )
         transition_workspace.clear("logprob_predict")
 
         # We start several optimization epochs on mini_batches
         for opt_epoch in range(cfg.algorithm.opt_epochs):
             if cfg.algorithm.minibatch_size > 0:
-                sample_workspace = transition_workspace.sample_subworkspace(1, cfg.algorithm.minibatch_size, 2)
+                sample_workspace = transition_workspace.sample_subworkspace(
+                    1, cfg.algorithm.minibatch_size, 2
+                )
             else:
                 sample_workspace = transition_workspace
 
-            actor(sample_workspace, t=0, n_steps=1, compute_entropy=True, predict_proba=True)
+            actor(
+                sample_workspace,
+                t=0,
+                n_steps=1,
+                compute_entropy=True,
+                predict_proba=True,
+            )
 
             advantage, action_logp, old_action_logp, entropy = sample_workspace[
-                "advantage",
-                "logprob_predict",
-                "old_action_logprobs",
-                "entropy"
+                "advantage", "logprob_predict", "old_action_logprobs", "entropy"
             ]
 
-            critic_loss = compute_critic_loss(advantage)  # issue here, can be used only once
+            critic_loss = compute_critic_loss(
+                advantage
+            )  # issue here, can be used only once
             adv_actor = advantage.detach().squeeze(0)[0]
             act_diff = action_logp[0] - old_action_logp[0]
             ratios = act_diff.exp()
 
             kl_agent(sample_workspace, t=0, n_steps=1)
             kl = sample_workspace["kl"][0]
-            actor_loss = compute_agent_loss(
-                cfg, adv_actor, ratios, kl
-            )
+            actor_loss = compute_agent_loss(cfg, adv_actor, ratios, kl)
 
             # Entropy loss favor exploration
             entropy_loss = torch.mean(entropy[0])
@@ -254,8 +258,8 @@ def run_ppo_v1(cfg):
             loss_critic = cfg.algorithm.critic_coef * critic_loss
 
             loss = (
-                    - cfg.algorithm.actor_coef * actor_loss
-                    - cfg.algorithm.entropy_coef * entropy_loss
+                -cfg.algorithm.actor_coef * actor_loss
+                - cfg.algorithm.entropy_coef * entropy_loss
             )
 
             old_policy.copy_parameters(policy)
@@ -318,7 +322,13 @@ def run_ppo_v1(cfg):
                 directory = f"./ppo_agent/{cfg.gym_env.env_name}/"
                 if not os.path.exists(directory):
                     os.makedirs(directory)
-                filename = directory + cfg.gym_env.env_name + "#ppo_kl#team#" + str(mean.item()) + ".agt"
+                filename = (
+                    directory
+                    + cfg.gym_env.env_name
+                    + "#ppo_kl#team#"
+                    + str(mean.item())
+                    + ".agt"
+                )
                 policy.save_model(filename)
                 if cfg.plot_agents:
                     plot_policy(
@@ -337,6 +347,7 @@ def run_ppo_v1(cfg):
                         best_reward,
                     )
 
+
 @hydra.main(
     config_path="./configs/",
     # config_name="ppo_lunarlander_continuous.yaml",
@@ -346,7 +357,6 @@ def run_ppo_v1(cfg):
     # config_name="ppo_cartpole.yaml",
     version_base="1.1",
 )
-
 def main(cfg: DictConfig):
     # print(OmegaConf.to_yaml(cfg))
     torch.manual_seed(cfg.algorithm.seed)

bbrl_examples/algos/tqc/tqc.py
@@ -52,22 +52,18 @@ def create_tqc_agent(cfg, train_env_agent, eval_env_agent):
 
     # Builds the critics
     critic = TruncatedQuantileNetwork(
-        obs_size, cfg.algorithm.architecture.critic_hidden_size,
-        cfg.algorithm.architecture.n_nets, act_size,
-        cfg.algorithm.architecture.n_quantiles
+        obs_size,
+        cfg.algorithm.architecture.critic_hidden_size,
+        cfg.algorithm.architecture.n_nets,
+        act_size,
+        cfg.algorithm.architecture.n_quantiles,
     )
     target_critic = copy.deepcopy(critic)
 
     train_agent = TemporalAgent(tr_agent)
     eval_agent = TemporalAgent(ev_agent)
     train_agent.seed(cfg.algorithm.seed)
-    return (
-        train_agent,
-        eval_agent,
-        actor,
-        critic,
-        target_critic
-    )
+    return (train_agent, eval_agent, actor, critic, target_critic)
 
 
 def make_gym_env(env_name):
@@ -106,12 +102,14 @@ def setup_entropy_optimizers(cfg):
 
 
 def compute_critic_loss(
-        cfg, reward, must_bootstrap,
-        t_actor,
-        q_agent,
-        target_q_agent,
-        rb_workspace,
-        ent_coef
+    cfg,
+    reward,
+    must_bootstrap,
+    t_actor,
+    q_agent,
+    target_q_agent,
+    rb_workspace,
+    ent_coef,
 ):
     # Compute quantiles from critic with the actions present in the buffer:
     # at t, we have Qu  ntiles(s,a) from the (s,a) in the RB
@@ -131,26 +129,36 @@ def compute_critic_loss(
         post_quantiles = rb_workspace["quantiles"][1]
 
         sorted_quantiles, _ = torch.sort(post_quantiles.reshape(quantiles.shape[0], -1))
-        quantiles_to_drop_total = cfg.algorithm.top_quantiles_to_drop * cfg.algorithm.architecture.n_nets
-        truncated_sorted_quantiles = sorted_quantiles[:,
-                                     :quantiles.size(-1) * quantiles.size(-2) - quantiles_to_drop_total]
+        quantiles_to_drop_total = (
+            cfg.algorithm.top_quantiles_to_drop * cfg.algorithm.architecture.n_nets
+        )
+        truncated_sorted_quantiles = sorted_quantiles[
+            :, : quantiles.size(-1) * quantiles.size(-2) - quantiles_to_drop_total
+        ]
 
         # compute the target
         logprobs = ent_coef * action_logprobs_next[1]
-        y = reward[0].unsqueeze(-1) + must_bootstrap.int().unsqueeze(-1) * cfg.algorithm.discount_factor * (
-                    truncated_sorted_quantiles - logprobs.unsqueeze(-1))
+        y = reward[0].unsqueeze(-1) + must_bootstrap.int().unsqueeze(
+            -1
+        ) * cfg.algorithm.discount_factor * (
+            truncated_sorted_quantiles - logprobs.unsqueeze(-1)
+        )
 
     # computing the Huber loss
-    pairwise_delta = y[:, None, None, :] - quantiles[:, :, :, None]  # batch x nets x quantiles x samples
+    pairwise_delta = (
+        y[:, None, None, :] - quantiles[:, :, :, None]
+    )  # batch x nets x quantiles x samples
 
     abs_pairwise_delta = torch.abs(pairwise_delta)
-    huber_loss = torch.where(abs_pairwise_delta > 1,
-                             abs_pairwise_delta - 0.5,
-                             pairwise_delta ** 2 * 0.5)
+    huber_loss = torch.where(
+        abs_pairwise_delta > 1, abs_pairwise_delta - 0.5, pairwise_delta**2 * 0.5
+    )
 
     n_quantiles = quantiles.shape[2]
     tau = torch.arange(n_quantiles).float() / n_quantiles + 1 / 2 / n_quantiles
-    loss = (torch.abs(tau[None, None, :, None] - (pairwise_delta < 0).float()) * huber_loss).mean()
+    loss = (
+        torch.abs(tau[None, None, :, None] - (pairwise_delta < 0).float()) * huber_loss
+    ).mean()
     return loss
 
 
@@ -170,9 +178,10 @@ def compute_actor_loss(ent_coef, t_actor, q_agent, rb_workspace):
     q_agent(rb_workspace, t=0, n_steps=1)
     quantiles = rb_workspace["quantiles"][0]
 
-    actor_loss = (ent_coef * action_logprobs_new[0] - quantiles.mean(2).mean(1))
+    actor_loss = ent_coef * action_logprobs_new[0] - quantiles.mean(2).mean(1)
 
     return actor_loss.mean()
+
 
 def run_tqc(cfg):
     # 1)  Build the  logger
@@ -184,13 +193,9 @@ def run_tqc(cfg):
     train_env_agent, eval_env_agent = create_env_agents(cfg)
 
     # 3) Create the A2C Agent
-    (
-        train_agent,
-        eval_agent,
-        actor,
-        critic,
-        target_critic
-    ) = create_tqc_agent(cfg, train_env_agent, eval_env_agent)
+    (train_agent, eval_agent, actor, critic, target_critic) = create_tqc_agent(
+        cfg, train_env_agent, eval_env_agent
+    )
 
     t_actor = TemporalAgent(actor)
     q_agent = TemporalAgent(critic)
@@ -251,15 +256,20 @@ def run_tqc(cfg):
             # See https://colab.research.google.com/drive/1erLbRKvdkdDy0Zn1X_JhC01s1QAt4BBj?usp=sharing
             must_bootstrap = torch.logical_or(~done[1], truncated[1])
 
-            critic_loss = compute_critic_loss(cfg, reward, must_bootstrap,
-                                              t_actor, q_agent, target_q_agent,
-                                              rb_workspace, ent_coef)
+            critic_loss = compute_critic_loss(
+                cfg,
+                reward,
+                must_bootstrap,
+                t_actor,
+                q_agent,
+                target_q_agent,
+                rb_workspace,
+                ent_coef,
+            )
 
             logger.add_log("critic_loss", critic_loss, nb_steps)
 
-            actor_loss = compute_actor_loss(
-                ent_coef, t_actor, q_agent, rb_workspace
-            )
+            actor_loss = compute_actor_loss(ent_coef, t_actor, q_agent, rb_workspace)
             logger.add_log("actor_loss", actor_loss, nb_steps)
 
             # Entropy coef update part #####################################################
@@ -269,7 +279,7 @@ def run_tqc(cfg):
                 # see https://github.com/rail-berkeley/softlearning/issues/60
                 ent_coef = torch.exp(log_entropy_coef.detach())
                 entropy_coef_loss = -(
-                        log_entropy_coef * (action_logprobs_rb + target_entropy)
+                    log_entropy_coef * (action_logprobs_rb + target_entropy)
                 ).mean()
                 entropy_coef_optimizer.zero_grad()
                 # We need to retain the graph because we reuse the
@@ -325,7 +335,13 @@ def run_tqc(cfg):
                 directory = f"./agents/{cfg.gym_env.env_name}/tqc_agent/"
                 if not os.path.exists(directory):
                     os.makedirs(directory)
-                filename = directory + cfg.gym_env.env_name + "#tqc#team" + str(mean.item()) + ".agt"
+                filename = (
+                    directory
+                    + cfg.gym_env.env_name
+                    + "#tqc#team"
+                    + str(mean.item())
+                    + ".agt"
+                )
                 actor.save_model(filename)
                 """ Not ready for visualizing quantile policies and critic
                 if False:  # cfg.plot_agents:
@@ -345,6 +361,7 @@ def run_tqc(cfg):
                         best_reward,
                     )
                 """
+
 
 @hydra.main(
     config_path="./configs/",

bbrl_examples/models/actors.py
@@ -11,12 +11,13 @@ from bbrl.agents.agent import Agent
 
 
 class BaseActor(Agent, ABC):
-    """ Generic class to centralize copy_parameters"""
+    """Generic class to centralize copy_parameters"""
 
     def copy_parameters(self, other):
         """Copy parameters from other agent"""
         for self_p, other_p in zip(self.parameters(), other.parameters()):
             self_p.data.copy_(other_p)
+
 
 class ProbAgent(Agent):
     def __init__(self, state_dim, hidden_layers, n_action):
@@ -152,7 +153,7 @@ class TunableVarianceContinuousActor(BaseActor):
 
     def get_distribution(self, obs: torch.Tensor):
         mean = self.model(obs)
-        return Independent(Normal(mean, self.soft_plus(self.std_param[: ,0])), 1)
+        return Independent(Normal(mean, self.soft_plus(self.std_param[:, 0])), 1)
 
     def forward(
         self, t, stochastic=False, predict_proba=False, compute_entropy=False, **kwargs
@@ -200,13 +201,13 @@ class TunableVarianceContinuousActorExp(BaseActor):
         return Independent(Normal(mean, torch.exp(std)), 1)
 
     def forward(
-            self,
-            t,
-            *,
-            stochastic=True,
-            predict_proba=False,
-            compute_entropy=False,
-            **kwargs,
+        self,
+        t,
+        *,
+        stochastic=True,
+        predict_proba=False,
+        compute_entropy=False,
+        **kwargs,
     ):
         obs = self.get(("env/env_obs", t))
         dist = self.get_distribution(obs)
@@ -331,8 +332,8 @@ class ContinuousDeterministicActor(BaseActor):
 
 class ActorAgent(Agent):
     """Choose an action (either according to p(a_t|s_t) when stochastic is true,
-               or with argmax if false.
-            """
+    or with argmax if false.
+    """
 
     def __init__(self):
         super().__init__()

bbrl_examples/models/critics.py
@@ -79,8 +79,11 @@ class TruncatedQuantileNetwork(Agent):
         self.is_q_function = True
         self.nets = []
         for i in range(n_nets):
-            net = build_mlp([state_dim + action_dim] + list(hidden_layers) + [n_quantiles], activation=nn.ReLU())
-            self.add_module(f'qf{i}', net)
+            net = build_mlp(
+                [state_dim + action_dim] + list(hidden_layers) + [n_quantiles],
+                activation=nn.ReLU(),
+            )
+            self.add_module(f"qf{i}", net)
             self.nets.append(net)
 
     def forward(self, t):